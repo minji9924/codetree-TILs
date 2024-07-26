a1341f365ebaa499f203cb80f5a13b6082fa56f6
Minji <58546758+minji9924@users.noreply.github.com>
2024-07-26 14:16:36 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void PrintStar(int n){
+    for(int i=0;i<n;i++){
+        cout << "12345^&*()_";
+    cout << "\n";
+    }
+}
+
+int main() {
+    int a;
+    cin >> a;
+    PrintStar(a);
+    return 0;
+}

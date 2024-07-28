bc7604548d6958f6cf032871e90baa2d5d1d3d97
Minji <58546758+minji9924@users.noreply.github.com>
2024-07-28 23:45:50 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240728/Factorial/factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int F(int n){
+    if(n<=1)
+        return 1;
+
+    return F(n-1)*n;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << F(n);
+    return 0;
+}

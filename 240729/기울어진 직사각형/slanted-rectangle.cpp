c59a0e8c0d5b6b97a6b5158523b83c1b888d85c2
Minji <58546758+minji9924@users.noreply.github.com>
2024-07-29 13:11:50 +0900

    Title: 기울어진 직사각형, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240729/기울어진 직사각형/slanted-rectangle.cpp
+++ 240729/기울어진 직사각형/slanted-rectangle.cpp
@@ -19,7 +19,7 @@ int main() {
         // 시작 위치가 i, j일 때
         if (check_start(i, j)) {
             // 1, 3번 방향으로 몇번 움직일지
-            int k1 = min(i-1, j); 
+            int k1 = min(i-1, N-j); 
             for (int x = 1; x <= k1; x++) {
                 // 2, 4번 방향으로 몇번 움직일지
                 int k2 = min(i-k1, j+k1); 
@@ -28,12 +28,14 @@ int main() {
                     for (int k = 0; k <= k1; k++) {
                         sum += board[i-k][j+k];
                         sum += board[i-k2-k][j-k2+k];
+                        // cout << board[i-k][j+k] << ' ' << board[i-k2-k][j-k2+k];
                     }
-                    for (int k = 1; k <= k2; k++) {
+                    for (int k = 1; k < k2; k++) {
                         sum += board[i-k][j-k];
                         sum += board[i-k1-k][j-k1-k];
+                        // cout << board[i-k][j-k] << ' ' << board[i-k1-k][j-k1-k];
                     }
-                    cout << "i, j, k1, k2, ans: " << i << ' ' <<  j << ' ' << x << ' ' << y << ' ' << sum <<'\n';
+                    // cout << "i, j, k1, k2, ans: " << i << ' ' <<  j << ' ' << x << ' ' << y << ' ' << sum <<'\n';
                     answer = max(sum, answer);
                 }
             }

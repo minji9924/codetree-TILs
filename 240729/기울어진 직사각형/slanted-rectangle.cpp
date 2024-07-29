815d3fe27f4a0781ae8288502da65230146ad9d4
Minji <58546758+minji9924@users.noreply.github.com>
2024-07-29 14:12:46 +0900

    Title: 기울어진 직사각형, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240729/기울어진 직사각형/slanted-rectangle.cpp
+++ 240729/기울어진 직사각형/slanted-rectangle.cpp
@@ -16,33 +16,33 @@ int main() {
     for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) cin >> board[i][j];
 
     for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) {
-        // 시작 위치가 i, j일 때
-        if (check_start(i, j)) {
-            // 1, 3번 방향으로 몇번 움직일지
-            int k1 = min(i-1, N-j-1); 
-            for (int x = 1; x <= k1; x++) {
-                // 2, 4번 방향으로 몇번 움직일지
-                int k2 = min(i-x-1, j+x-1); 
-                for (int y = 1; y <= k2; y++) {
-                    int sum = 0;
-                    
-                    for (int k = 0; k <= x; k++) {
-                        sum += board[i-k][j+k];
-                        sum += board[i-y-k][j-y+k];
-                        
+            // 시작 위치가 i, j일 때
+            if (check_start(i, j)) {
+                // 1, 3번 방향으로 몇번 움직일지
+                int k1 = min(i-1, N-j-1);
+                for (int x = 1; x <= k1; x++) {
+                    // 2, 4번 방향으로 몇번 움직일지
+                    int k2 = min(i-x, j+x-1);
+                    for (int y = 1; y <= k2; y++) {
+                        int sum = 0;
+                        if (i-x-y >= 0 && j + x < N && j - y >= 0) {
+                            for (int k = 0; k <= x; k++) {
+                                sum += board[i-k][j+k];
+                                sum += board[i-y-k][j-y+k];
+
+                            }
+                            for (int k = 1; k < y; k++) {
+                                sum += board[i-k][j-k];
+                                sum += board[i-x-k][j+x-k];
+
+                            }
+                        }
+//                        cout << "i, j, k1, k2, ans: " << i << ' ' <<  j << ' ' << x << ' ' << y << ' ' << sum <<'\n';
+                        answer = max(sum, answer);
                     }
-                    for (int k = 1; k < y; k++) {
-                        sum += board[i-k][j-k];
-                        sum += board[i-x-k][j+x-k];
-                        
-                    }
-                    
-                    // cout << "i, j, k1, k2, ans: " << i << ' ' <<  j << ' ' << x << ' ' << y << ' ' << sum <<'\n';
-                    answer = max(sum, answer);
                 }
             }
         }
-    }
     cout << answer;
     return 0;
 }

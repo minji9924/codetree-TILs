8bf350afabbbdbf7363f7687b937271df972c104
Minji <58546758+minji9924@users.noreply.github.com>
2024-07-29 20:03:31 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/단어 정렬/sorting-words.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string a[n];
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    sort(a,a+n);
+    for(int i=0;i<n;i++){
+        cout << a[i] << "\n";
+    }
+    return 0;
+}

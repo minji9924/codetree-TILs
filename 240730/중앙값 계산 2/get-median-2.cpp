8ce52ceb1464a9c1a58e900f8f9aaad23f34d7bd
Minji <58546758+minji9924@users.noreply.github.com>
2024-07-30 14:01:24 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240730/중앙값 계산 2/get-median-2.cpp
+++ 240730/중앙값 계산 2/get-median-2.cpp
@@ -9,16 +9,12 @@ int main() {
     int sum=0;
     for(int i=1;i<=n;i++){
         cin >> arr[i];
-        // sum+= arr[i];
-        // if(i%2!=0)
     }
-    sort(arr+1,arr+n+1);
     for(int i=1;i<=n;i++){
-        sum+= arr[i];
-        if(i%2!=0)
-            cout << sum/i << ' ';
+        if(i%2!=0){
+            sort(arr+1,arr+i+1);
+            cout << arr[(i+1)/2] << ' ';    
+        }
     }
-
-
     return 0;
 }

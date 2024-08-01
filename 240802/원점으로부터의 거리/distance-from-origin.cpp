22e24ad84bc447be18d68108c9ef857eb5f9d646
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-02 02:39:49 +0900

    Title: 원점으로부터의 거리, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240802/원점으로부터의 거리/distance-from-origin.cpp
+++ 240802/원점으로부터의 거리/distance-from-origin.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 class P{
@@ -16,8 +17,9 @@ class P{
 P p[1000];
 
 bool cmp(P p1, P p2){
-    if(p1.x+p1.y != p2.x+p2.y)
-        return p1.x+p1.y < p2.x+p2.y;
+    if(abs(p1.x)+abs(p1.y) != abs(p2.x)+abs(p2.y))
+        return abs(p1.x)+abs(p1.y) < abs(p2.x)+abs(p2.y);
+    return p1.n < p2.n;
 }
 
 int main() {

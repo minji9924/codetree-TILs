31bde86f9dea2f80a2759d27c3ce25f180c8182c
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-04 23:19:57 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240804/요일 맞추기/guess-day-of-week.cpp
+++ 240804/요일 맞추기/guess-day-of-week.cpp
@@ -7,7 +7,17 @@ int main() {
     cin >> m1 >> d1 >> m2 >> d2;
     int month_2011[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
     string day[7] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
-    int sum = (month_2011[m2]+d2)-(month_2011[m1]+d1);
+    int sum_2=0, sum_1=0;
+    int i=1,j=1;
+    while(i != m1){
+        sum_1 += month_2011[i++];
+    }
+    sum_1 +=d1;
+    while(j != m2){
+        sum_2 += month_2011[j++];
+    }
+    sum_2 +=d2;
+    int sum = sum_2 - sum_1;
     if(sum>=0){
         cout << day[sum%7];
     }

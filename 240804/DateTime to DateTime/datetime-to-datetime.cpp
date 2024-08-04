bccc053e5aa2cad123145dff21c39edf62955cdf
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-04 22:56:17 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240804/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240804/DateTime to DateTime/datetime-to-datetime.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int a,b,c;
     cin >> a >> b >> c;
-    if((a<11)||(a>=11 && b<11)||(a>=11 && b>=11 & c<11))
+    if((a<11)||(a==11 && b<11)||(a==11 && b==11 & c<11))
         cout << -1;
     int sum = (a*1440+b*60+c) - 11*(1440+60+1);
     cout << sum;

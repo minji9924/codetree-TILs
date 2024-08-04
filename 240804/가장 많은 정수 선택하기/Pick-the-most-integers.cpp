adaaa1816ec1d749756701b2692ac72c4e3dfa65
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-04 23:36:28 +0900

    Title: 가장 많은 정수 선택하기, Time: 69ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240804/가장 많은 정수 선택하기/Pick-the-most-integers.cpp
+++ 240804/가장 많은 정수 선택하기/Pick-the-most-integers.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
-#include <set>
+#include <map>
 #include <algorithm>
 using namespace std;
 
 int N, D, K, C, diff_num, answer;
 int number[300001];
-set<int> cont_num;
+map<int, int> cont_num;
 
 int main() {
     cin >> N >> D >> K >> C;
     for (int i = 0; i < N; i++) cin >> number[i];
 
-    for (int i = 0; i < K; i++) cont_num.insert(number[i]);
+    for (int i = 0; i < K; i++) cont_num.emplace(number[i], 1);
+
+    if (cont_num[C] > 0) cont_num[C]++;
+    else cont_num[C] = 1;
 
     for (int i = K; i < N + K; i++) {
-        cont_num.insert(C);
+
         int size = cont_num.size();
         answer = max(answer, size);
-        cont_num.erase(number[i-K]);
-        cont_num.insert(number[i]);
+
+        if (cont_num[number[i-K]] == 1) cont_num.erase(number[i-K]);
+        else cont_num[number[i-K]]--;
+
+        if (cont_num[number[i%N]] > 0) cont_num[number[i%N]]++;
+        else cont_num[number[i%N]] = 1;
+
     }
 
     cout << answer;

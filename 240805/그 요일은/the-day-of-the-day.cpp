f0a07947aa923100e6cfbaab6079d9511f386ec1
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-05 22:24:49 +0900

    Title: 그 요일은, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240805/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int m1,d1,m2,d2;
+    int month_2024[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
+    cin >> m1 >> d1 >> m2 >> d2;
+    string day;
+    string day_2024[7] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+    cin >> day;
+    int cnt=0,i=i,sum=0;
+    while(1){
+        if(day == day_2024[cnt])
+            break;
+        cnt++;
+    }
+    while(1){
+        if(month_2024[])
+     
+        day+= day+cnt*i;
+
+    }
+    return 0;
+}

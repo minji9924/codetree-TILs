b919e96245a997697b7ddb4e756206d38fe5c022
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-06 14:53:52 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240806/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> v;
+    v.resize(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> v[i][j];
+        }
+    }
+
+    int answer = 0;
+
+    // 행 확인
+    for (int i = 0; i < n; i++) {
+        int tmp = 1;
+        for (int j = 0; j < n - 1; j++) {
+            if (v[i][j] == v[i][j + 1]) tmp++;
+            else tmp = 1;
+
+            if (tmp >= m) {
+                answer++;
+                break;
+            }
+        }
+    }
+
+    // 열 확인
+    for (int i = 0; i < n; i++) {
+        int tmp = 1;
+        for (int j = 0; j < n - 1; j++) {
+            if (v[j][i] == v[j + 1][i]) tmp++;
+            else tmp = 1;
+
+            if (tmp >= m) {
+                answer++;
+                break;
+            }
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}

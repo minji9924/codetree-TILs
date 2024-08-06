a0fcd908a8985fd69d235d56055997d482dc2e5b
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-06 14:57:07 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240806/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -18,32 +18,37 @@ int main()
 
     int answer = 0;
 
-    // 행 확인
-    for (int i = 0; i < n; i++) {
-        int tmp = 1;
-        for (int j = 0; j < n - 1; j++) {
-            if (v[i][j] == v[i][j + 1]) tmp++;
-            else tmp = 1;
+    if (m != 1) {
+        // 행 확인
+        for (int i = 0; i < n; i++) {
+            int tmp = 1;
+            for (int j = 0; j < n - 1; j++) {
+                if (v[i][j] == v[i][j + 1]) tmp++;
+                else tmp = 1;
 
-            if (tmp >= m) {
-                answer++;
-                break;
+                if (tmp >= m) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+
+        // 열 확인
+        for (int i = 0; i < n; i++) {
+            int tmp = 1;
+            for (int j = 0; j < n - 1; j++) {
+                if (v[j][i] == v[j + 1][i]) tmp++;
+                else tmp = 1;
+
+                if (tmp >= m) {
+                    answer++;
+                    break;
+                }
             }
         }
     }
-
-    // 열 확인
-    for (int i = 0; i < n; i++) {
-        int tmp = 1;
-        for (int j = 0; j < n - 1; j++) {
-            if (v[j][i] == v[j + 1][i]) tmp++;
-            else tmp = 1;
-
-            if (tmp >= m) {
-                answer++;
-                break;
-            }
-        }
+    else {
+        answer = 2 * n;
     }
 
     cout << answer;

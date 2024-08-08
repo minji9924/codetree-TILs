ae125520f70497fa5cba7acc93bc06cc3f2848e4
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-08 21:15:51 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240808/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ 240808/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    int n,x1,y1,x2,y2,s[300][300]={0},sum=0;
+    int n,x1,y1,x2,y2,s[300][300]={0},sum=0,of=100;
     cin >> n;
     for(int i=0;i<n;i++){
         cin >> x1 >> y1 >> x2 >> y2;
         for(int p=x1;p<x2;p++){
             for(int q=y1;q<y2;q++){
-                if(s[p][q]!=1){
-                    s[p][q]++;
+                if(s[of+p][of+q]!=1){
+                    s[of+p][of+q]++;
                     sum++;
                 }
             }

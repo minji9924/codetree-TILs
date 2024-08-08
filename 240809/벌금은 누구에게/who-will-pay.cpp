dcb95328c7b34820f3b1c1a5d79fc18a4c44ef94
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-09 06:15:16 +0900

    Title: 벌금은 누구에게, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240809/벌금은 누구에게/who-will-pay.cpp
+++ 240809/벌금은 누구에게/who-will-pay.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main() {
-    int n,m,k,a[101]={0},st,tmp=-1;
+    int n,m,k,a[100]={0},st,tmp=-1;
     cin >> n >> m >> k;
-    for(int i=1;i<=m;i++){
+    for(int i=0;i<m;i++){
         cin >> st;
         a[st]++;
         if(a[st]==k){
             tmp=st;
+            break;
         }
     }
     cout << tmp;

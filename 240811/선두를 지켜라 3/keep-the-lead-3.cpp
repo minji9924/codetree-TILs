7cd2a7e87c790f7ea6f5ad6af9e3062e902f305b
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-11 16:55:23 +0900

    Title: 선두를 지켜라 3, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240811/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m,v,t,loc=0,time=0,flag=0,cnt=0;
+    int pos_a[1002]={0};
+    int pos_b[1002]={0};
+    
+    cin >> n >> m;
+    for(int i=0;i<n;i++){
+        cin >> v >> t;
+        time+=t;
+        while(t--){
+            loc++;
+            pos_a[loc]=pos_a[loc-1]+v;
+        }
+    }
+    loc=0;
+    for(int i=0;i<m;i++){
+        cin >> v >> t;
+        while(t--){
+            loc++;
+            pos_b[loc]=pos_b[loc-1]+v;
+        }
+    }
+
+    for(int i=1;i<time;i++){
+        if((pos_a[i]==pos_b[i]) && flag!=1){
+            cnt++;
+            flag=1;
+            }
+       else if((pos_a[i]>pos_b[i]) && flag!=2){
+            cnt++;
+            flag=2;
+       }
+       else if((pos_b[i]>pos_a[i]) && (flag!=3)){
+            cnt++;
+            flag=3;
+    }
+    }
+    cout << cnt;
+    return 0;
+}

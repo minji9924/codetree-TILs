7963f9f0238697cfb53f681055aeda467ea710b2
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-12 16:46:59 +0900

    Title: 되돌아오기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240812/되돌아오기/come-back.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+
+bool InRange(int x,int y,int n){
+    return (x>=0 && x<n && y>=0 && y<n);
+}
+
+int DirNum(char a){
+    if(a=='E')
+        return 0;
+    else if(a=='W')
+        return 1;
+    else if(a=='N')
+        return 2;
+    else
+        return 3;
+}
+
+int main() {
+    int n,dist,dir_num,x=0,y=0,cnt=0,flag=0;
+    int s[100][100]={0};
+    char dir;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        cin >> dir >> dist;
+        dir_num=DirNum(dir);
+        for(int j=0;j<dist;j++){
+            if(InRange(x+dx[dir_num],y+dy[dir_num],n)){
+                x=x+dx[dir_num];
+                y=y+dy[dir_num];
+                cnt++;
+                if(x==0 && y==0){
+                    flag=1;
+                    break;
+                }
+                s[x][y]=cnt;
+            }
+        }
+        if(flag)
+            break;
+    }
+    if(flag)
+        cout << cnt;
+    else
+        cout << -1;
+
+    return 0;
+}

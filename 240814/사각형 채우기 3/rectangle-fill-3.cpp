fa72a51de4e9b89fb6bd79a54b7b513c3c27815f
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-14 10:56:27 +0900

    Title: 사각형 채우기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240814/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240814/사각형 채우기 3/rectangle-fill-3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int N, dp[1001];
+long long N, dp[1001];
 
 int main() {
     cin >> N;
@@ -9,8 +9,9 @@ int main() {
     dp[0] = 1; dp[1] = 2; dp[2] = 7;
     // 여기에 코드를 작성해주세요.
     for (int i = 3; i <= N; i++) {
-        dp[i] = dp[i-1] * 2 + dp[i-2] * 3 + dp[i-3] * 2;
+        dp[i] = ((dp[i-1] * 2) % 1000000007 + (dp[i-2] * 3) % 1000000007 + (dp[i-3] * 2) % 1000000007) % 1000000007;
     }      
+    1000000007
     cout << dp[N];
     return 0;
 }

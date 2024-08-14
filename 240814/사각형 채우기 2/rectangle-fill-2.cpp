6a4779976fb5ba135dd23bef99bd84d1ed7b58a3
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-14 14:00:51 +0900

    Title: 사각형 채우기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240814/사각형 채우기 2/rectangle-fill-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int N, dp[1001];
+
+int main() {
+    cin >> N;
+    dp[1] = 1; dp[2] = 3; 
+    for (int i = 3; i <= N; i++) {
+        dp[i] = (dp[i-2] * 2 + dp[i-1]) % 10007;
+    }
+    cout << dp[N];
+    return 0;
+}

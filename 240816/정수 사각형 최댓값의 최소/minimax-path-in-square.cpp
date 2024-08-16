c240f2d0ee19c424ce4fcfd4b201119b53af3aed
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-16 12:21:20 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240816/정수 사각형 최댓값의 최소/minimax-path-in-square.cpp
+++ 240816/정수 사각형 최댓값의 최소/minimax-path-in-square.cpp
@@ -10,7 +10,7 @@ int main() {
         cin >> board[i][j];
     }
     dp[0][0] = board[0][0];
-    for (int i = 1; i < 8; i++) {
+    for (int i = 1; i < N; i++) {
         dp[i][0] = max(dp[i-1][0], board[i][0]);
         dp[0][i] = max(dp[0][i-1], board[0][i]);
     }

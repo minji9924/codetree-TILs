67d655fe6580c764f20aacc0449a06a7d288cbb2
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-16 23:34:24 +0900

    Title: 2차원 최대 증가 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240816/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 240816/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -6,37 +6,32 @@ using namespace std;
 
 int N, M, board[51][51], dp[51][51];
 
-int in_range(int x, int y) {
-    if (x < 0 || y < 0 || x >= N || y >= M) return false;
-    return true;
-}
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
 
     cin >> N >> M;
-    for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++) for (int j = 0; j < M; j++) {
         cin >> board[i][j];
     }
     fill(&dp[0][0], &dp[50][51], MIN);
 
     dp[0][0] = 1;
 
-    for (int i = 1; i < N; i++) for (int j = 1; j < N; j++) {
+    for (int i = 1; i < N; i++) for (int j = 1; j < M; j++) {
         int dest = board[i][j];
         for (int x = 0; x < i; x++) for (int y = 0; y < j; y++) {
             if (board[x][y] < dest && dp[x][y] != MIN) {
-                dp[i][j] = dp[x][y] + 1;
+                dp[i][j] = max(dp[i][j], dp[x][y] + 1);
             }
         }
     }
     int answer = 0;
-    for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++) for (int j = 0; j < M; j++) {
         if (dp[i][j] != MIN) {
             answer = max(answer, dp[i][j]);
         }
     }
-    
+
     cout << answer;
     return 0;
 }

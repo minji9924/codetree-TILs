88d38df775d62f08293ddb95ff2fb86ba8c78039
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-16 19:14:36 +0900

    Title: 최대 감소 부분 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240816/최대 감소 부분 수열/longest-decreasing-subsequence.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int N, num[1001], dp[1001];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> num[i];
+
+    fill(&dp[0], &dp[1000], 1); // 초기화!
+    dp[0] = 1;
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            if (num[j] > num[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+    
+    int answer = 0;
+    for (int i = 0; i < N; i++) answer = max(answer, dp[i]);
+    cout << answer;
+
+    return 0;
+}

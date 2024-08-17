73c12459f6e6b4d64ef76635f1dfc7d043c7465a
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-17 21:35:51 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240817/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 240817/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -10,18 +10,21 @@ int IsRange(int r,int c,int n){
 
 int main() {
     int n,r,c,cnt=1;
-    int s[100][100]={0},dir=0;
+    int s[100][100]={0},dir=0,tmp=0,tmp2=1,a=1;
     cin >> n;
     r=(n-1)/2;
     c=(n-1)/2;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            s[r][c]=cnt++;
-            if(!IsRange(r+dr[dir],c+dc[dir],n) || s[r+dr[dir]][c+dc[dir]]!=0)
-                dir=(dir+1)%4;
-            r=r+dr[dir];
-            c=c+dc[dir];
-        }
+    for(int i=1;i<=n*n;i++){
+            if(a>=(n*2))
+                break;
+            tmp=(a+1)/2;
+            for(int j=0;j<(a+1)/2;j++){
+                s[r][c]=cnt++;
+                r=r+dr[dir];
+                c=c+dc[dir];
+            }
+            dir=(dir+1)%4;
+            a++;
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){

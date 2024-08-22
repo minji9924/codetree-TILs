6c6a4c5e2f13f5f449db5ded163bc765e9070fc2
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-22 22:07:46 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240822/모이자/gather.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,p[102],sum=0,mn=1000;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> p[i];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            sum+=abs(i-j)*(p[j]);
+        }
+        mn=min(sum,mn);
+        sum=0;
+    }
+    cout << mn;
+    return 0;
+}

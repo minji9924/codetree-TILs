e13635c6438c7513a284d7f3f218156be04669d7
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-22 17:23:04 +0900

    Title: 바이러스 검사, Time: 361ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 240822/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<int> v;
+int x, y;
+int cnt;
+
+int main() 
+{
+    cin >> n;
+
+    v.resize(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    cin >> x >> y;
+
+    // 팀장 확인
+    for (int i = 0; i < n; i++) {
+        if (v[i] <= x) {
+            cnt++;
+            v[i] = 0;
+        }
+        else {
+            cnt++;
+            v[i] -= x;
+        }
+    }
+
+    // 팀원 확인
+    for (int i = 0; i < n; i++) {
+        if (v[i] <= 0) continue;
+
+        cnt += v[i] / y;
+        
+        if (v[i] % y > 0) cnt++;
+    }
+
+    cout << cnt;
+
+    return 0;
+}

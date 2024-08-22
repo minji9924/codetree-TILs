0dd0b7888f23bca4f432a6d362e891b0c0730dc4
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-22 23:17:09 +0900

    Title: 최고의 13위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240822/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,a[21][21]={0},sum=0,mx=0;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-2;j++){
+            if(a[i][j]==1)
+                sum++;
+            if(a[i][j+1]==1)
+                sum++;
+            if(a[i][j+2]==1)
+                sum++;
+            mx=max(sum,mx);
+        }
+        sum=0;
+    }
+    cout << mx;
+    return 0;
+}

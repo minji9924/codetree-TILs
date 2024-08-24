56deacca90b5df917b31476a5123d4deb3cc322a
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-24 13:46:58 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240824/Carry 피하기 2/escaping-carry-2.cpp
+++ 240824/Carry 피하기 2/escaping-carry-2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int n,tmp1=0,tmp2=0,tmp3=0,tmp4=0,mx=0,sum=0;
+    int n,tmp1=0,tmp2=0,tmp3=0,tmp4=0,mx=-1,sum=0;
     int s[25];
     cin >> n;
     for(int i=0;i<n;i++){
@@ -16,10 +16,11 @@ int main() {
                 tmp2=(s[i]%100)/10+(s[j]%100)/10+(s[k]%100)/10;
                 tmp3=(s[i]%1000)/100+(s[j]%1000)/100+(s[k]%1000)/100;
                 tmp4=(s[i]%10000)/1000+(s[j]%10000)/1000+(s[k]%10000)/1000;
-                if(tmp1<10 && tmp2<10 && tmp3<10 && tmp4<10)
+                if(tmp1<10 && tmp2<10 && tmp3<10 && tmp4<10){
                     sum=s[i]+s[j]+s[k];
-                mx=max(sum,mx);
-                sum=0;
+                    mx=max(sum,mx);
+                    sum=0;
+                }
             }
         }
     }

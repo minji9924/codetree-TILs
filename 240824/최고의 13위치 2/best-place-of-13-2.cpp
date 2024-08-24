bfad34d44d19d49b80793c6df0d673530e0a720f
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-24 20:32:39 +0900

    Title: 최고의 13위치 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240824/최고의 13위치 2/best-place-of-13-2.cpp
+++ 240824/최고의 13위치 2/best-place-of-13-2.cpp
@@ -28,7 +28,7 @@ int main() {
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<n-2;j++){
-            if((((j+2)>=r) || (j<=(r+2))) && r==i) continue;
+            if((((j+2)>=c) && (j<=(c+2))) && r==i) continue;
             else{
                 if(a[i][j]==1)
                     sum2++;

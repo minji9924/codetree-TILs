8163b99ec3e714c19c6130df41d5153cd51abb2a
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-26 00:30:51 +0900

    Title: 아름다운 수열 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240826/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 240826/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int n,m,ans=0,cnt=0;
-    int a[101],b[101];
+    int a[101],b[101],ack[101]={0},bck[101]={0};
     cin >> n >> m;
     for(int i=0;i<n;i++)
         cin >> a[i];
@@ -12,12 +12,20 @@ int main() {
     for(int i=0;i<=n-m;i++){
         for(int j=0;j<m;j++){
             for(int k=i;k<m+i;k++){
-                if(a[k]==b[j]){
+                if(a[k]==b[j] && !bck[j] && !ack[k]){
                     cnt++;
+                    bck[j]=1;
+                    ack[k]=1;
                     break;
                 }
             }
         }
+        for(int j=0;j<m;j++){
+            bck[j]=0;
+        }
+        for(int k=i;k<m+i;k++){
+            ack[k]=0;
+        }        
         if(cnt==m)
             ans++;
         cnt=0;

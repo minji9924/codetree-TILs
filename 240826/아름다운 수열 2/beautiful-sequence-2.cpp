d3403229383f17d222d9bb3a22479c02b167ffdf
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-26 00:00:14 +0900

    Title: 아름다운 수열 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240826/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m,ans=0,cnt=0;
+    int a[101],b[101];
+    cin >> n >> m;
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+    for(int i=0;i<m;i++)
+        cin >> b[i];
+    for(int i=0;i<=n-m;i++){
+        for(int j=0;j<m;j++){
+            for(int k=i;k<m+i;k++){
+                if(a[k]==b[j]){
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        if(cnt==m)
+            ans++;
+        cnt=0;
+    }
+    cout << ans;
+    return 0;
+}

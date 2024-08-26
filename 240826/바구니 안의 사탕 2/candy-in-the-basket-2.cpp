508f36a306fa602d5e46f9e6343ae414d6cc5102
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-26 13:40:43 +0900

    Title: 바구니 안의 사탕 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240826/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 240826/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -7,13 +7,13 @@ using namespace std;
 // 사탕 개수, 바구니 좌표
 
 int main() {
-    int n,k,cnd,x,bsk[500]={0},mx=0,sum=0;
+    int n,k,cnd,x,bsk[10000]={0},mx=0,sum=0;
     cin >> n >> k;
     for(int i=0;i<n;i++){
         cin >> cnd >> x;
         bsk[x]+=cnd;
     }
-    for(int i=k;i<MAX_K-k-1;i++){
+    for(int i=k;i<5000-k-1;i++){
         for(int j=i-k;j<=i+k;j++){
             sum+=bsk[j];
         }

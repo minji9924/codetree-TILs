20b6ed2a1a4b4ef9b3d17f4c566278ae92448ad3
Minji <58546758+minji9924@users.noreply.github.com>
2024-08-27 23:32:02 +0900

    Title: 삼각형 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240827/삼각형 만들기/create-triangle.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n,x[101],y[101],ext,mx_ext=INT_MIN;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> x[i] >> y[i];
+    }
+    for(int i=0;i<n-1;i++){
+        int dst_x=0;
+        int dst_y=0;
+        int dst_mxx=INT_MIN;
+        int dst_mxy=INT_MIN;
+        for(int j=0;j<n;j++){
+            if(x[i]==x[j]){
+                dst_y=abs(y[j]-y[i]);
+                dst_mxy=max(dst_y,dst_mxy);
+            }
+            if(y[i]==y[j]){
+                dst_x=abs(x[j]-x[i]);
+                dst_mxx=max(dst_x,dst_mxx);
+            }            
+        }
+        ext=dst_mxx*dst_mxy;
+        mx_ext=(mx_ext,ext);
+    }
+    cout << mx_ext;
+    return 0;
+}
